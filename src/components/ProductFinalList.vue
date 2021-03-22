d4e29c5de0069919af8994f01a98e41f6fe10391
MeOneIRL <fahmi.alwan@gmail.com>
2021-03-22 21:47:48 +0700

    add payment page

---
+++ src/components/ProductFinalList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-card elevation="0">
+      <v-row class="mx-1 mt-5">
+        <div>
+          <v-img height="80" max-width="120" :src="listFinalCart.image"></v-img>
+        </div>
+        <div>
+          <v-card-title class="font-weight-medium text-subtitle-1">
+            {{ listFinalCart.productName }}
+          </v-card-title>
+          <v-card-subtitle class="text-subtitle-2">
+            {{ parseRupiah(listFinalCart.hargaBaru) }}
+          </v-card-subtitle>
+        </div>
+        <v-spacer></v-spacer>
+        <div>{{ listFinalCart.qty }}</div>
+      </v-row>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["listFinalCart"],
+  data() {
+    return {};
+  },
+  methods: {
+    parseRupiah(strMoney) {
+      return parseInt(strMoney).toLocaleString("id", {
+        style: "currency",
+        currency: "IDR"
+      });
+    }
+  }
+};
+</script>
+
+<style></style>

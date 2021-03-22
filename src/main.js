66806746b82258cf1e15bf55b4992fbecf5153e3
MeOneIRL <fahmi.alwan@gmail.com>
2021-03-22 21:49:57 +0700

    editing dependencies

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+// import VueSplide from "@splidejs/vue-splide";
 
 Vue.config.productionTip = false;
+
+// Vue.use(VueSplide);
 
 new Vue({
   router,

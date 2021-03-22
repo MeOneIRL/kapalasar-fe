2730fbd5ee059344cb9b0240dff2cbab563a043e
MeOneIRL <fahmi.alwan@gmail.com>
2021-03-22 21:50:29 +0700

    add dummy data

---
+++ src/dummyData/dummy.js
@@ -0,0 +1,197 @@
+export const products = [
+  {
+    id: 1,
+    productName: "flash",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: true
+  },
+  {
+    id: 2,
+    productName: "flash",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: true
+  },
+  {
+    id: 3,
+    productName: "flash",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: true
+  },
+  {
+    id: 4,
+    productName: "flash",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: true
+  },
+  {
+    id: 5,
+    productName: "flash",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: true
+  },
+  {
+    id: 6,
+    productName: "Sayuran A",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 7,
+    productName: "Sayuran A",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 8,
+    productName: "Sayuran A",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 9,
+    productName: "Daging A",
+    description: "100 gram/ pack",
+    kategori: "daging",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 10,
+    productName: "Buah A",
+    description: "100 gram/ pack",
+    kategori: "buah",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 11,
+    productName: "Bumbu A",
+    description: "100 gram/ pack",
+    kategori: "bumbu",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 12,
+    productName: "Sayuran A",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 13,
+    productName: "Sayuran A",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 14,
+    productName: "Sayuran A",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  },
+  {
+    id: 15,
+    productName: "Sayuran A",
+    description: "100 gram/ pack",
+    kategori: "sayuran",
+    image: require("../assets/sayuran.jpg"),
+    rating: 4,
+    stock: "100",
+    hargaCoret: "10000",
+    hargaBaru: "5000",
+    flashSale: false
+  }
+];
+
+export const filters = [
+  { src: require("../assets/Semua.png"), name: "semua" },
+  { src: require("../assets/Buah.png"), name: "buah" },
+  { src: require("../assets/Daging.png"), name: "daging" },
+  { src: require("../assets/Sayur.png"), name: "sayur" },
+  { src: require("../assets/Bumbu.png"), name: "bumbu" }
+];
+
+export const slides = [
+  { src: "https://picsum.photos/800/800" },
+  { src: "https://picsum.photos/800/800" },
+  { src: "https://picsum.photos/800/800" },
+  { src: "https://picsum.photos/800/800" }
+];

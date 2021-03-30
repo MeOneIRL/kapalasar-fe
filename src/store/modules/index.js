2fa81d3abb924dc93cc6784a6e1901c4fca2913a
MeOneIRL <fahmi.alwan@gmail.com>
2021-03-31 01:23:08 +0700

    add transaction state

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,9 +1,11 @@
 import cart from "./cart";
 import user from "./user";
 import voucher from "./voucher";
+import transaction from "./transaction";
 
 export default {
   cart,
   user,
-  voucher
+  voucher,
+  transaction
 };
